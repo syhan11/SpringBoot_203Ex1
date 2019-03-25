f833b68d43a60501a6744cb63d6dfe7b01342944
GBTC440014UR <GBTC440014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-25 19:02:13 -0400

    Initial commit

---
+++ src/main/java/com/bysue/springboot_203ex1/Employee.java
@@ -0,0 +1,40 @@
+package com.bysue.springboot_203ex1;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private long ssn;
+    private String dob;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
---
+++ src/main/java/com/bysue/springboot_203ex1/Springboot203ex1Application.java
@@ -0,0 +1,13 @@
+package com.bysue.springboot_203ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot203ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot203ex1Application.class, args);
+	}
+
+}
